fd2a499409e0673f424b335a290c0799613b03c8
bellalouise9937920 <96032634+bellalouise9937920@users.noreply.github.com>
2023-03-02 17:53:33 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {View,
+TouchableOpacity,
+Text,
+StyleSheet,} from 'react-native'
+import {Component} from 'react'
+import {SafeAreaProvider} from 'react-native-safe-area-context'
+import Button from './telas/botoes.js'
+export default class App extends Component {
+  render () {
+    return (
+      <Button>
+
+      </Button>
+    )
+  }
+}
